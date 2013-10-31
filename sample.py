149438a6f542f41ad67c9f05531b345bd1d0c8d0
fredsilva <fredsilva.sistemas@gmail.com>
2013-10-31 15:02:06 -0300

    Criacao da Funcao sample_reduce

--- sample.py
+++ sample.py
@@ -1,9 +1,13 @@
 # encoding: utf-8
+from functools import reduce
 
 def sample_map():
 	''' Trabalhando com Map '''
 	return list(map(lambda x: x**2, range(1,100)))
 
+def sample_reduce():
+	''' Trabalhando com Reduce'''
+	return reduce(lambda x, y: x+y, range(1, 100))
 
 if __name__ == '__main__':
-	print(sample_map())
+	print(sample_reduce())
